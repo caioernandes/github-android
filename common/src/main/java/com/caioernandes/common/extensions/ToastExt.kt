5041ec1db99646079b7eea385bfdaae84c8b4476
Caio Ernandes <84975402+caio-nascimento-picpay@users.noreply.github.com>
2022-10-13 16:51:19 -0300

    remove extensions nao utilizadas

--- common/src/main/java/com/caioernandes/common/extensions/ToastExt.kt
+++ common/src/main/java/com/caioernandes/common/extensions/ToastExt.kt
@@ -1,16 +1,7 @@
 package com.caioernandes.common.extensions
 
-import android.app.Activity
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-
-fun Activity.showLongToast(text: String) {
-    Toast.makeText(this, text, Toast.LENGTH_LONG).show()
-}
-
-fun Activity.showShortToast(text: String) {
-    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
-}
 
 fun Fragment.showLongToast(text: String) {
     Toast.makeText(requireContext(), text, Toast.LENGTH_LONG).show()
